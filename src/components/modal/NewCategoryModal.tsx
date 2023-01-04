31caeb21b977162ee820a7905a408adf064224f4
Narei <ieranofficial@gmail.com>
2023-01-04 20:00:23 +0100

    modal layout changes

--- src/components/modal/NewCategoryModal.tsx
+++ src/components/modal/NewCategoryModal.tsx
@@ -67,12 +67,12 @@ function NewCategoryModal({ toggleCategoryModal }: any) {
                 <div className='flex flex-col gap-1'>
                     <p className='text-sm uppercase font-semibold text-stone-400'>Tables</p>
 
-                    <div className='my-2 flex flex-col gap-2'>
+                    <div className='my-6 md:w-1/2 flex flex-col gap-2'>
                         {tables.map(table => {
                             return (<div key={table.id} className='flex w-full justify-between items-center'>
                                 <div className='flex gap-2 items-center'>
-                                    <div className='h-7 w-7 rounded-full' style={{backgroundColor: table.color}}></div>
-                                    <p className='uppercase font-semibold'>{table.name}</p>
+                                    <div className='h-5 w-5 rounded-full' style={{backgroundColor: table.color}}></div>
+                                    <p>{table.name}</p>
                                 </div>
                                 <svg onClick={() => handleTableDelete(table.id)} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-5 h-5 cursor-pointer">
                                     <path strokeLinecap="round" strokeLinejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
@@ -82,16 +82,16 @@ function NewCategoryModal({ toggleCategoryModal }: any) {
                     </div>
 
                     <div className='flex w-full flex-col md:flex-row gap-2'>
-                        <input type="text" className='w-full md:w-1/2 bg-stone-200 px-2 py-1 rounded-xl placeholder-stone-400' placeholder='Table name' ref={tableName} />
-                        <div className='w-full md:w-1/2 flex gap-2'>
+                        <input type="text" className='w-full bg-stone-200 px-2 py-1 rounded-xl placeholder-stone-400' placeholder='Table name' ref={tableName} />
+                        <div className='flex gap-2'>
                             <input type="color" className='invisible w-0 h-full bg-stone-200 rounded-xl' ref={tableColor} onChange={e => setCurrentColor(e.target.value)} />
-                            <button onClick={() => (tableColor.current as any).click()} className='w-1/2 text-stone-100 rounded-xl flex justify-center bg-blue-500 items-center gap-2 uppercase font-semibold text-xs' style={{backgroundColor: currentColor}}>
+                            <button onClick={() => (tableColor.current as any).click()} className='px-4 w-1/2 md:w-max text-stone-100 rounded-xl flex justify-center bg-blue-500 items-center gap-2 uppercase font-semibold text-xs' style={{backgroundColor: currentColor}}>
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-5 h-5">
                                     <path strokeLinecap="round" strokeLinejoin="round" d="M15.042 21.672L13.684 16.6m0 0l-2.51 2.225.569-9.47 5.227 7.917-3.286-.672zM12 2.25V4.5m5.834.166l-1.591 1.591M20.25 10.5H18M7.757 14.743l-1.59 1.59M6 10.5H3.75m4.007-4.243l-1.59-1.59" />
                                 </svg>
                                 <p>Pick color</p>
                             </button>
-                            <button onClick={(event) => handleTableSubmit(event)} className='text-xs w-1/2 uppercase font-semibold bg-blue-500 py-2 px-4 text-stone-100 rounded-xl hover:bg-blue-500/90 flex gap-2 items-center justify-center'>
+                            <button onClick={(event) => handleTableSubmit(event)} className='text-xs w-1/2 md:w-max uppercase font-semibold bg-blue-500 py-2 px-4 text-stone-100 rounded-xl hover:bg-blue-500/90 flex gap-2 items-center justify-center'>
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-5 h-5">
                                     <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
                                 </svg>
@@ -102,7 +102,7 @@ function NewCategoryModal({ toggleCategoryModal }: any) {
                 </div>
 
                 <hr />
-                <div className='flex items-center justify-end'>
+                <div className='w-full gap-2 flex items-center justify-end'>
                     <button onClick={() => createCategory()} className='text-xs uppercase font-semibold bg-blue-500 py-2 px-4 text-stone-100 rounded-xl hover:bg-blue-500/90 flex gap-2 items-center'>
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-5 h-5">
                             <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
